ba832fb6b6414c2e0922023dadbc108f55bfd7eb
Unknown <vdvinh@apcs.vn>
2017-05-02 11:59:49 +0700

    [WIP] adapt sst

--- dataset.py
+++ dataset.py
@@ -84,3 +84,83 @@ class SICKDataset(data.Dataset):
             labels = map(lambda x: float(x), f.readlines())
             labels = torch.Tensor(labels)
         return labels
+
+# Dataset class for SICK dataset
+class SSTDataset(data.Dataset):
+    def __init__(self, path, vocab, num_classes):
+        super(SSTDataset, self).__init__()
+        self.vocab = vocab
+        self.num_classes = num_classes
+
+        self.sentences = self.read_sentences(os.path.join(path,'sents.toks'))
+
+        self.trees = self.read_trees(os.path.join(path,'dparents.txt'))
+
+        self.labels = self.read_labels(os.path.join(path,'dlabels.txt'))
+
+        self.size = self.labels.size(0)
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, index):
+        # ltree = deepcopy(self.ltrees[index])
+        # rtree = deepcopy(self.rtrees[index])
+        # lsent = deepcopy(self.lsentences[index])
+        # rsent = deepcopy(self.rsentences[index])
+        # label = deepcopy(self.labels[index])
+        tree = deepcopy(self.trees[index])
+        sent = deepcopy(self.sentences[index])
+        label = deepcopy(self.labels[index])
+        return (tree, sent, label)
+
+    def read_sentences(self, filename):
+        with open(filename,'r') as f:
+            sentences = [self.read_sentence(line) for line in tqdm(f.readlines())]
+        return sentences
+
+    def read_sentence(self, line):
+        indices = self.vocab.convertToIdx(line.split(), Constants.UNK_WORD)
+        return torch.LongTensor(indices)
+
+    def read_trees(self, filename):
+        # TODO: change to read sst
+        with open(filename,'r') as f:
+            trees = [self.read_tree(line) for line in tqdm(f.readlines())]
+        return trees
+
+    def read_tree(self, line):
+        parents = map(int,line.split())
+        trees = dict()
+        root = None
+        for i in xrange(1,len(parents)+1):
+            #if not trees[i-1] and parents[i-1]!=-1:
+            if i-1 not in trees.keys() and parents[i-1]!=-1:
+                idx = i
+                prev = None
+                while True:
+                    parent = parents[idx-1]
+                    if parent == -1:
+                        break
+                    tree = Tree()
+                    if prev is not None:
+                        tree.add_child(prev)
+                    trees[idx-1] = tree
+                    tree.idx = idx-1
+                    #if trees[parent-1] is not None:
+                    if parent-1 in trees.keys():
+                        trees[parent-1].add_child(tree)
+                        break
+                    elif parent==0:
+                        root = tree
+                        break
+                    else:
+                        prev = tree
+                        idx = parent
+        return root
+
+    def read_labels(self, filename):
+        with open(filename,'r') as f:
+            labels = map(lambda x: float(x), f.readlines())
+            labels = torch.Tensor(labels)
+        return labels
