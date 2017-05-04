5dd1a6fccda05c832d1ffec43b8ce0d54f623454
Unknown <vdvinh@apcs.vn>
2017-05-04 13:22:25 +0700

    fix bug fh fx, change default weight decay
    wd default=0.5*1e-4

--- config.py
+++ config.py
@@ -34,11 +34,11 @@ def parse_args(type=0):
                             help='directory with GLOVE embeddings')
         parser.add_argument('--batchsize', default=25, type=int,
                             help='batchsize for optimizer updates')
-        parser.add_argument('--epochs', default=15, type=int,
+        parser.add_argument('--epochs', default=200, type=int,
                             help='number of total epochs to run')
         parser.add_argument('--lr', default=0.05, type=float,
                             metavar='LR', help='initial learning rate')
-        parser.add_argument('--wd', default=1e-4, type=float,
+        parser.add_argument('--wd', default=0.5*1e-4, type=float,
                             help='weight decay (default: 1e-4)')
         parser.add_argument('--optim', default='adagrad',
                             help='optimizer (default: adagrad)')
