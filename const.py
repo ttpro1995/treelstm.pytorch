1e5b7c68b3ee38e3adb4ba9babb5385783cddc75
Thai Thien <tthien@apcs.vn>
2017-05-15 14:51:49 +0700

    mlp for comLSTM
    - feed word embedding, tag embedding and rel embedding into multilayer mlp

--- const.py
+++ const.py
@@ -1,4 +1,7 @@
 attention_hid_dim = 150
+mlp_com_hid_dim = 150
+mlp_com_out_dim = 150
+mlp_num_hid_layer = 3
 p_dropout_input = 0.5
 p_dropout_memory = 0.1
 attention = False
@@ -8,3 +11,4 @@ def show_setting():
         print('Attention hidden dim: ' + str(attention_hid_dim))
     print('Dropout input: ' + str(p_dropout_input))
     print('Dropout memory: ' + str(p_dropout_memory))
+    print('Compositional MLP: ' + str(mlp_com_hid_dim) + ' ' + str(mlp_num_hid_layer) + ' ' +  str(mlp_com_out_dim))
