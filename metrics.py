b98689d5ccc3c28f95075329af926dd86641af05
Thai Thien <tthien@apcs.vn>
2017-05-24 19:08:12 +0700

    metrics to print depth

--- metrics.py
+++ metrics.py
@@ -33,6 +33,8 @@ class SubtreeMetric():
     def __init__(self):
         self.correct = {}
         self.total = {}
+        self.correct_depth = {}
+        self.total_depth = {}
 
     def reset(self):
         self.correct = {}
@@ -46,12 +48,46 @@ class SubtreeMetric():
             self.correct[height] = 0
         if correct:
             self.correct[height] += 1
+    def count_depth(self, correct, depth):
+        if depth in self.total_depth.keys():
+            self.total_depth[depth] +=1
+        else:
+            self.total_depth[depth] = 1
+            self.correct_depth[depth] = 0
+        if correct:
+            self.correct_depth[depth] += 1
 
     def getAcc(self):
         acc = {}
         for height in self.total.keys():
             acc[height] = float(self.correct[height]) / self.total[height]
         return acc
+
+    def getAccDepth(self, start, end = -1):
+        if end == -1:
+            acc = {}
+            for depth in self.total_depth.keys():
+                acc[depth] = float(self.correct[depth]) / self.total[depth]
+            return acc
+        else:
+            total = 0
+            correct = 0
+            acc = {}
+            for depth in range(start, end+1):
+                if depth in self.total_depth.keys():
+                    acc[depth] = float(self.correct_depth[depth]) / self.total_depth[depth]
+                    total += self.total_depth[depth]
+                    correct += self.correct_depth[depth]
+                else:
+                    acc[depth] = 0
+            group_acc = float(correct)/total
+            return acc, group_acc
+
+    def printAccDepth(self, start, end = -1):
+        acc, group_acc = self.getAccDepth(start, end)
+        for key in acc.keys():
+            print ('Depth ' + str(key) +' '+ str(self.correct_depth[key]) +'/'+ str(self.total_depth[key]) +' acc ' + str(acc[key]))
+
 
     def printAcc(self):
         acc = self.getAcc()
@@ -68,3 +104,15 @@ if __name__ == "__main__":
 
     acc = metric.getAcc()
     metric.printAcc()
+
+    metric.count_depth(True, 0)
+    metric.count_depth(False, 0)
+    metric.count_depth(True, 1)
+    metric.count_depth(True, 2)
+    metric.count_depth(True, 3)
+    metric.count_depth(True, 4)
+    metric.count_depth(True, 5)
+    metric.count_depth(True, 5)
+    metric.count_depth(False, 5)
+    acc, group_acc = metric.getAccDepth(0, 5)
+    metric.printAccDepth(0,5)
